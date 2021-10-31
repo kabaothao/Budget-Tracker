94c3412ed87576b46c9388b13bece2dfdd0c81c9
Kabao Thao <tkabao@gmail.com>
2021-10-31 17:19:23 -0500

    service worker updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,6 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+//TODO: set up const files to cache
+//TODO: set up to install. add event listener function
+//TODO: set up fetch function 
