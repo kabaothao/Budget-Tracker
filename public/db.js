422557083e1249b4d7e76723161168a1da1089c6
Kabao Thao <tkabao@gmail.com>
2021-11-06 17:32:02 -0500

    update readme

--- public/db.js
+++ public/db.js
@@ -2,20 +2,20 @@
 let db;
   //1 means it is Version 1. It is saying it is a version for what it needs to be store.
     // create a new db request for a "BudgetDB" database.
-const request = indexDB.open("budget", 1);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
     // create object store called "BudgetStore" and set autoIncrement to true
-        const db = event.target.results;
+        const db = event.target.result;
         db.createObjectStore("BudgetStore", {autoIncrement: true});
   };
 
   request.onsuccess = function (event) {
-      console.log("'success")
+      console.log("success")
     db = event.target.result;
     //set up to check if app is online before reading from db 
     if (navigator.onLine) {
-        console.log('Backend onlinee!')
+        console.log('Backend online is working!')
       checkDatabase();
     }
   };
